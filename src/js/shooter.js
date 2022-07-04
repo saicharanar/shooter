b5e5c3ab89eeb79adf3c913bb683f4366efa772d
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-07-04 18:00:25 +0530

    Introduced view class and styles.css

--- src/js/shooter.js
+++ src/js/shooter.js
@@ -1,6 +1,39 @@
 (function () {
+  const px = (val) => `${val}px`;
+
+  class View {
+    #dimensions;
+    constructor(dimensions) {
+      this.#dimensions = dimensions;
+    }
+
+    get dimensions() {
+      const { left, top, right, bottom } = this.#dimensions;
+      return { left, top, right, bottom };
+    }
+
+    get randomPoint() {
+      const { left, top, right, bottom } = this.#dimensions;
+      const x = Math.ceil(Math.random() * right - left);
+      const y = Math.ceil(Math.random() * bottom - top);
+      return { x, y };
+    }
+  }
+
+  const drawScreen = (view) => {
+    const { left, top, right, bottom } = view.dimensions;
+    const screen = document.createElement('screen');
+    screen.id = 'screen';
+    screen.style.left = px(left);
+    screen.style.top = px(top);
+    screen.style.width = px(right - left);
+    screen.style.height = px(bottom - top);
+    document.body.appendChild(screen);
+  };
+
   const startGame = () => {
-    alert('started');
+    const view = new View({ left: 80, top: 80, right: 1800, bottom: 800 });
+    drawScreen(view);
   };
 
   window.onload = startGame;
