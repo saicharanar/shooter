17bee1fa62973b6010683470bf90ed559d16e6d5
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-07-04 18:40:08 +0530

    Introduced player with movements

--- src/js/shooter.js
+++ src/js/shooter.js
@@ -20,7 +20,39 @@
     }
   }
 
-  const drawScreen = (view) => {
+  class Player {
+    #src;
+    #position;
+    #speed;
+    constructor(src, position, speed) {
+      this.#src = src;
+      this.#position = position;
+      this.#speed = speed;
+    }
+
+    getCoordinates() {
+      const { x, y } = this.#position;
+      return { x, y };
+    }
+
+    moveUp() {
+      this.#position.y -= this.#speed.dy;
+    }
+
+    moveDown() {
+      this.#position.y += this.#speed.dy;
+    }
+
+    moveLeft() {
+      this.#position.x -= this.#speed.dx;
+    }
+
+    moveRight() {
+      this.#position.x += this.#speed.dx;
+    }
+  }
+
+  const createScreen = (view) => {
     const { left, top, right, bottom } = view.dimensions;
     const screen = document.createElement('screen');
     screen.id = 'screen';
@@ -29,11 +61,66 @@
     screen.style.width = px(right - left);
     screen.style.height = px(bottom - top);
     document.body.appendChild(screen);
+    return screen;
+  };
+
+  const updatePlayerPos = (event, player) => {
+    const { key } = event;
+    if (key === 'ArrowUp') {
+      player.moveUp();
+    }
+
+    if (key === 'ArrowDown') {
+      player.moveDown();
+    }
+
+    if (key === 'ArrowLeft') {
+      player.moveLeft();
+    }
+
+    if (key === 'ArrowRight') {
+      player.moveRight();
+    }
+  };
+
+  const drawPlayer = (screen, player) => {
+    const { x, y } = player.getCoordinates();
+    const playerSprite = document.createElement('img');
+    playerSprite.id = 'player';
+    playerSprite.style.left = x;
+    playerSprite.style.top = y;
+    screen.appendChild(playerSprite);
+  };
+
+  const createPlayer = () => {
+    const player = new Player('_', { x: 250, y: 100 }, { dx: 3, dy: 3 });
+    return player;
+  };
+
+  const removeAllChildren = (screen) => {
+    const children = screen.children;
+    for (const child of children) {
+      screen.removeChild(child);
+    }
+  };
+
+  const redrawScreen = (screen, player) => {
+    if (screen.children) {
+      removeAllChildren(screen);
+    }
+    drawPlayer(screen, player);
   };
 
   const startGame = () => {
     const view = new View({ left: 80, top: 80, right: 1800, bottom: 800 });
-    drawScreen(view);
+    const screen = createScreen(view);
+    const player = createPlayer();
+    drawPlayer(screen, player);
+
+    window.onkeydown = (event) => {
+      updatePlayerPos(event, player);
+      redrawScreen(screen, player);
+    };
   };
 
   window.onload = startGame;
